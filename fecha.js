af9f8ccd32b53274d6ad6aa9c4e546517175bd6c
Guille <guichezero@gmail.com>
2021-02-17 03:17:14 -0300

    Update fecha.js

--- fecha.js
+++ fecha.js
@@ -1,10 +1,9 @@
-
-function timeElapsed(last_date='May 27, 2020 00:00:00'){
+function timeElapsed(x_date='May 25, 2021 00:00:00'){
     /**
      * @author hwpoison <github.com/hwpoison>
      */
-  const t_now = new Date()                // actual date
-  const t_past = new Date(last_date) // last date
+  const t_now = new Date()              // actual date
+  const unk_date = new Date(x_date) 	// undefined date
 
   const time_struct = (timeDate)=>{
     return {
@@ -17,15 +16,17 @@ function timeElapsed(last_date='May 27, 2020 00:00:00'){
     }
     // time flags
     const actual = time_struct(t_now)
-    const past = time_struct(t_past)
-    const same_day = actual.day == past.day
-    const same_month = actual.month == past.month
-    const same_year = actual.year == past.year
-    let mins_d = (actual.min - past.min)
-    const hour_d = (actual.hour - past.hour)
-    const day_d = (actual.day - past.day)
-    const month_d = (actual.month - past.month)
-    const year_d = (actual.year - past.year)
+    const unk =   time_struct(unk_date)
+
+    const same_day =   actual.day == unk.day
+    const same_month = actual.month == unk.month
+    const same_year =  actual.year == unk.year
+
+    let   mins_d =    (actual.min - unk.min)
+    const hour_d =    (actual.hour - unk.hour)
+    const day_d =     (actual.day - unk.day)
+    const month_d =   (actual.month - unk.month)
+    const year_d =    (actual.year - unk.year)
 
     // output messages
     const messages = {
@@ -33,49 +34,57 @@ function timeElapsed(last_date='May 27, 2020 00:00:00'){
       lastMonth:    'El mes pasado',
       lastYear:     'El año pasado',
       lastDay:      'Ayer',
-      inMonths:     'Hace $months meses',
-      inYears:      'Hace $year años',
-      inDays:       'Hace $day días',
-      inMins:       'Hace $min minutos',
-      inHours:      'Hace $hour horas',
-      inFuture:     'Algun tiempo en el futuro'
+      agoMonths:    'Hace $months meses',
+      agoYears:     'Hace $year años',
+      agoDays:      'Hace $day días',
+      lastMins:     'Hace $min minutos',
+      lastHours:    'Hace $hour horas',
+      inSomeMonths: 'En $month meses',
+      inSomeDays:   'En $day días.',
+      inFuture:     'Algun tiempo en el futuro',
     }
 
     // Today
     if(same_day && same_month && same_year){
-      if((actual.hour) == past.hour){
-        if(actual.min == past.min){
+      if((actual.hour) == unk.hour){
+        if(actual.min == unk.min){
           return messages.lastMinute
-        }else if(actual.min < past.min){
+        }else if(actual.min < unk.min){
           mins_d = 60 - (-mins_d)
         }
-          return messages.inMins.replace('$min', mins_d)
+          return messages.lastMins.replace('$min', mins_d)
         }else{
-          return messages.inHours.replace('$hour', hour_d)
+          return messages.lastHours.replace('$hour', hour_d)
         }
     // Last Day's
     }else if(same_month && same_year){
-      if(actual.day-1 == past.day){
+      if(actual.day-1 == unk.day){
         return messages.lastDay
       }else{
-        return messages.inDays.replace('$day', day_d)
+      	if(unk.day > actual.day){
+      		return messages.inSomeDays.replace('$day', day_d*-1)
+      	}
+        return messages.agoDays.replace('$day', day_d)
       }   
     // Last Month's
     }else if(same_year){
-      if(actual.month-1 == past.month){
+      if(actual.month-1 == unk.month){
         return messages.lastMonth
       }else{
-        return messages.inMonths.replace('$month', month_d)
+      	if(unk.month > actual.month){
+      		return messages.inSomeMonths.replace('$month', month_d*-1)
+      	}
+        return messages.agoMonths.replace('$month', month_d)
       } 
     // Last Year's and other time
     }else{
-      if(actual.year-1 == past.year){
+      if(actual.year-1 == unk.year){
         return messages.lastYear
       }else{
         if(year_d < 1){
           return messages.inFuture
         }else{
-          return messages.inYears.replace('$year', year_d)
+          return messages.agoYears.replace('$year', year_d)
         }
       }
     }
